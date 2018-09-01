72db99508a6db3a56fb0e9c71dd8c8af2e68671a
antsergon <antsergon@gmail.com>
2018-08-31 21:30:48 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/sergio/cursomc/domain/Categoria.java
+++ src/main/java/com/sergio/cursomc/domain/Categoria.java
@@ -2,10 +2,19 @@ package com.sergio.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{ //serializable converte a classe em bytes para trafegar/salvar em arq
 
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
